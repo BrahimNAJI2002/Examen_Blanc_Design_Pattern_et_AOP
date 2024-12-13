f9b98c6a5c82396721a860da42782f570904aa8d
BrahimNAJI2002 <brahim.naji1-etu@etu.univh2c.ma>
2024-12-13 10:27:14 +0100

    first commit

---
+++ src/main/java/naji/brahim/examen_blanc_design_pattern_et_aop/ExamenBlancDesignPatternEtAopApplication.java
@@ -0,0 +1,13 @@
+package naji.brahim.examen_blanc_design_pattern_et_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenBlancDesignPatternEtAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenBlancDesignPatternEtAopApplication.class, args);
+    }
+
+}
